5dd8fa00a4b334bde5be42f255e85f329c3a0921
mmexploit <mmexploit1@gmail.com>
2022-10-05 17:24:25 +0300

    started working on the react router

--- src/components/preview-cards/preview-cards.component.jsx
+++ src/components/preview-cards/preview-cards.component.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link, useHistory } from "react-router-dom";
 import './preview-cards.styles.css'
 
 const PreviewCard = ({ country }) => {
-    const { name : { official = "" } = {} , population = "", region = "", capital = "", flags : { svg = "" } = {}} = {} = country;
-
+    const { name : { official = "", common = "" } = {} , population = "", region = "", capital = "", flags : { svg = "" } = {}} = {} = country;
+    
     return (
+        <>
         <div className='preview-card'>
             <div className="flag" style={{ backgroundImage: `url(${svg})`}}/>
             <div className="description">
@@ -12,8 +14,11 @@ const PreviewCard = ({ country }) => {
                 <span className="population">Population: { population }</span>
                 <span className="region">Region: { region }</span>
                 <span className="capital">Capital: { capital[0] }</span>
+                <Link to="">See more</Link>
             </div>
         </div>
+        
+        </>
     )
 }
 
